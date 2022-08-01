847ca9101d01eca0b7e92ef495b3548062fc4fdc
Peter deHaan <peter@deseloper.com>
2022-08-01 08:40:54 -0700

    FIRST BLOOD

---
+++ src/_data/calendar/index.js
@@ -0,0 +1,16 @@
+module.exports = [
+  {
+    year: "2014",
+    month: "09",
+    posts: [
+      {"title": "Lorem Ipsum"}
+    ]
+  },
+  {
+    year: "2021",
+    month: "07",
+    posts: [
+      {title: "more stuff"}
+    ]
+  }
+];
